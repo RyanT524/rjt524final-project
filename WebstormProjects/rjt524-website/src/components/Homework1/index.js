e3c0b2462c0f77b57207f7cbf7a01241a92709e6
rjt524 <rjt524@vt.edu>
2022-07-01 01:29:07 -0400

    fixed aspect ratio for hw1

--- WebstormProjects/rjt524-website/src/components/Homework1/index.js
+++ WebstormProjects/rjt524-website/src/components/Homework1/index.js
@@ -3,11 +3,24 @@ import pic1 from "./hw1.PNG"
 import pic2 from "./hw1(2).PNG"
 const Homework1 = () => {
   return (
-    <div>
-      <h1 className={"head1"}>On Page load</h1>
-      <h2 className={"head2"}>After loading</h2>
-      <img src={pic1} className={"hw1img"} align={"middle"} width={"500"} height={"300"}/>
-      <img src={pic2} className={"hw1img2"} align={"middle"} width={"500"} height={"300"} />
+    <div className={"hw1margin"}>
+      <div>
+      <h1 className={"head1"}>On Page load and After Loading</h1>
+      </div>
+      {/*<div className={"column"}>*/}
+      {/*<img src={pic1} width={"500"} height={"300"}/>*/}
+      {/*</div>*/}
+      {/*<div className={"column"}>*/}
+      {/*<img src={pic2} width={"500"} height={"300"} />*/}
+      {/*</div>*/}
+      <div className={"row"}>
+        <div className={"column"}>
+          <img src={pic1} className={"pics"}/>
+        </div>
+        <div className={"column"}>
+          <img src={pic2} className={"pics"}/>
+        </div>
+      </div>
     </div>
   )
 }
